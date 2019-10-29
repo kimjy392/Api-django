09429755e3cf4a190d8a85dd5670ae469e882316
김준영 <kimjy392@gmail.com>
2019-10-29 10:25:00 +0900

    Music index, detail

---
+++ musics/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('musics/', views.index, name='index'),
+    path('musics/<int:music_pk>/', views.detail, name='detail'),
+]
--- musics/views.py
+++ musics/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Music
+from rest_framework.decorators import api_view
+from .serializers import MusicSerializers
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(['GET']) # HTTP method
+def index(request):
+    musics = Music.objects.all()
+    serializer = MusicSerializers(musics, many=True) # Query set 이라서 many=True!!
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def detail(request, music_pk):
+    music = get_object_or_404(Music, pk=music_pk)
+    serializer = MusicSerializers(music)
+    return Response(serializer.data)
